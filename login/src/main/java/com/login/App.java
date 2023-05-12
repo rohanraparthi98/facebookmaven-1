bbd160a6f5822a0d32519c63f4f0d6b9088add1b
abhinavchinna <abhi02111999@gmail.com>
2023-05-12 16:20:24 +0530

    added some changes in code

--- login/src/main/java/com/login/App.java
+++ login/src/main/java/com/login/App.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws InterruptedException {
 		 System.setProperty("webdriver.chrome.driver","../chromedriver.exe");
 		  WebDriver driver=new ChromeDriver();
 		  driver.get("http://www.facebook.com");
 		  driver.findElement(By.id("email")).sendKeys("yourUsername");
+		  Thread.sleep(5000);
 		  driver.findElement(By.id("pass")).sendKeys("yourPassword");
+		  Thread.sleep(5000);
 		  driver.findElement(By.name("login")).click();
 }
 }
